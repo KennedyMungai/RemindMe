7854f1f4b268208b9d4907b6e5e1645791faf994
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 02:23:05 +0300

    The basics of the CreateCollectionsSheet component

---
+++ components/Collections/CreateCollectionSheet.tsx
@@ -0,0 +1,12 @@
+import React, { SetStateAction } from "react";
+
+type Props = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+};
+
+const CreateCollectionSheet = ({ open, onOpenChange }: Props) => {
+  return <div>CreateCollectionSheet</div>;
+};
+
+export default CreateCollectionSheet;
