1a6711d61488776b8cd576edc07f8d9bed85b0f7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 15:09:02 +0300

    Created the Logo component

---
+++ components/NavBar/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Logo = (props: Props) => {
+  return (
+    <h1 className="text-2xl font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">
+      RemindMe
+    </h1>
+  );
+};
+
+export default Logo;
