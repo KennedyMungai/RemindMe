733138d195a44e5565cb322526c50ac53d25505c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 02:01:36 +0300

    The basic create collectio button component

---
+++ components/Collections/CreateCollectionButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CreateCollectionButton = (props: Props) => {
+  return (
+    <div>CreateCollectionButton</div>
+  )
+}
+
+export default CreateCollectionButton
