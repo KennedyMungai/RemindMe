c7f070bd1eaaed579716e79baefefcae33b38475
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 15:29:26 +0300

    Added some styles for the moon icon

--- components/NavBar/ThemeSwitcher.tsx
+++ components/NavBar/ThemeSwitcher.tsx
@@ -21,7 +21,7 @@ const ThemeSwitcher = () => {
           <SunIcon className="h-[1.2rem] w-[1.2rem]" />
         </TabsTrigger>
         <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
-          <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
+          <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0" />
         </TabsTrigger>
         <TabsTrigger value="system" onClick={() => setTheme("system")}>
           <DesktopIcon className="h-[1.2rem] w-[1.2rem]" />
