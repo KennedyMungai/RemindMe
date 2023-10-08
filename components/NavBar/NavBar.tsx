c56b248bf6d164bf881a95642686f05a8578e46e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 17:00:49 +0300

    Added some formatting for the Navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = (props: Props) => {
   return (
     <nav className="flex w-full items-center justify-between p-4 px-8 h-[10vh]">
       <Logo />
-      <div className="">
+      <div className="flex gap-5 items-center justify-around">
         <UserButton afterSignOutUrl="/ " />
         <ThemeSwitcher />
       </div>
