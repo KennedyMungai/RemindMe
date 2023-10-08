53577c5eea3f226a33c576e64d5e5a4886f3c100
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 02:07:47 +0300

    Returned a button

--- components/Collections/CreateCollectionButton.tsx
+++ components/Collections/CreateCollectionButton.tsx
@@ -1,10 +1,18 @@
 "use client";
 import React from "react";
+import { Button } from "../ui/button";
 
 type Props = {};
 
 const CreateCollectionButton = (props: Props) => {
-  return <div>CreateCollectionButton</div>;
+  return (
+    <Button
+      variant={"outline"}
+      className="dark:text-white w-full dark:bg-neutral-950 bg-white"
+    >
+      CreateCollectionButton
+    </Button>
+  );
 };
 
 export default CreateCollectionButton;
