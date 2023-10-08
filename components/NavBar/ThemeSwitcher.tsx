b97802bafa2aa187c9ba703bb9cc129f817339e6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 15:28:32 +0300

    Updated the size of the icons

--- components/NavBar/ThemeSwitcher.tsx
+++ components/NavBar/ThemeSwitcher.tsx
@@ -18,13 +18,13 @@ const ThemeSwitcher = () => {
     <Tabs defaultValue={theme}>
       <TabsList className="border dark:border-neutral-800 dark:bg-[#030303]">
         <TabsTrigger value="light" onClick={() => setTheme("light")}>
-          <SunIcon />
+          <SunIcon className="h-[1.2rem] w-[1.2rem]" />
         </TabsTrigger>
         <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
-          <MoonIcon />
+          <MoonIcon className="h-[1.2rem] w-[1.2rem]" />
         </TabsTrigger>
         <TabsTrigger value="system" onClick={() => setTheme("system")}>
-          <DesktopIcon />
+          <DesktopIcon className="h-[1.2rem] w-[1.2rem]" />
         </TabsTrigger>
       </TabsList>
     </Tabs>
