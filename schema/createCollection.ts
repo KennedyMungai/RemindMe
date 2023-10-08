b57b265ef586ae3cc3a3a3d050e726b744fe1bee
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 02:47:12 +0300

    Created the CreateCollectionSchemaType

--- schema/createCollection.ts
+++ schema/createCollection.ts
@@ -1,6 +1,11 @@
+import { CollectionColors } from "@/lib/constants";
 import { z } from "zod";
 
 export const createCollectionSchema = z.object({
   name: z.string().min(3, { message: "Name must be at least 3 characters" }),
-  color: z.string(),
+  color: z
+    .string()
+    .refine((color) => Object.keys(CollectionColors).includes(color)),
 });
+
+export type CreateCollectionSchemaType = z.infer<typeof createCollectionSchema>;
