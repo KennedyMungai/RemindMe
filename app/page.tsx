8bb97ede4c9d86a8fd4e8061229657ae3cd57731
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-08 16:32:07 +0300

    Added a 3 second delay for the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { wait } from "@/lib/wait";
 import { currentUser } from "@clerk/nextjs";
 import Error from "next/error";
 
 export default async function Home() {
   const user = await currentUser();
+  await wait(3000);
 
   if (!user) {
     return <div className="">Error</div>;
