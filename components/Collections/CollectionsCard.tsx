5d20ee3e59e6766211e4ed21a9b0381bec35329b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-09 11:23:08 +0300

    Wrote a more sophisticated CollectionsCard component

---
+++ components/Collections/CollectionsCard.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { Collection } from "@prisma/client";
+import React, { useState } from "react";
+import { Collapsible, CollapsibleTrigger } from "../ui/collapsible";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+import { CollectionColor, CollectionColors } from "@/lib/constants";
+
+type Props = {
+  collection: Collection;
+};
+
+const CollectionsCard = ({ collection }: Props) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+      <CollapsibleTrigger>
+        <Button
+          variant={"ghost"}
+          className={cn(
+            "flex w-full justify-between p-6",
+            CollectionColors[collection.color as CollectionColor]
+          )}
+        >
+          {collection.name}
+        </Button>
+      </CollapsibleTrigger>
+    </Collapsible>
+  );
+};
+
+export default CollectionsCard;
